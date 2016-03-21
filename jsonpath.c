4ee21a7a257624b2ab23feb3a903d83644576314
Mike Kaminski <michael.w.kaminski@gmail.com>
2016-03-21 07:53:41 -0400

    Support deep search

--- jsonpath.c
+++ jsonpath.c
@@ -111,6 +111,7 @@ void iterate(HashTable *arr, char * p, zval **data, zval * return_value)
 
     bool buildingKeyName = false;
     bool insideSubExpression = true;
+    HashPosition pos;
 
     while(*p != '\0') {
 
@@ -132,8 +133,21 @@ void iterate(HashTable *arr, char * p, zval **data, zval * return_value)
                 if(*(p+1) != '\0') {
                     switch(*(p+1)) {
                         case '.':
+                            p++;
+                            zval **tmp;
+                            char * tmp_p;
+                            HashTable *arr_tmp = arr;
+                            for(
+                                zend_hash_internal_pointer_reset_ex(arr, &pos);
+                                zend_hash_get_current_data_ex(arr, (void**) &tmp, &pos) == SUCCESS;
+                                zend_hash_move_forward_ex(arr, &pos)
+                            ) {
+                                tmp_p = p;
+                                arr_tmp = HASH_OF(*tmp);
+                                iterate(arr_tmp, tmp_p, tmp, return_value);
+                            }
 //                            iterate(arr, p, data, return_vale);
-                            break;
+                            return;
                         case '*':
 //                            printf("This is a wildcard match\n");
                             p++;
@@ -159,8 +173,10 @@ void iterate(HashTable *arr, char * p, zval **data, zval * return_value)
                         //Search the array for key
                         *curBuffer = '\0';
                         int len = strlen(buffer);
-                        zend_hash_find(arr, buffer, len+1, (void**)&data);
-                        add_next_index_zval(return_value, *data);
+                        if(arr != NULL && zend_hash_find(arr, buffer, len+1, (void**)&data) == SUCCESS) {
+                            PHPWRITE(buffer, len+1);
+                            add_next_index_zval(return_value, *data);
+                        }
                         return;
                     }
                 }
