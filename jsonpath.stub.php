250375455d3075cedc4edf48509b2691444c2c08
Viktor Djupsjöbacka <viktor.djupsjobacka@supermetrics.com>
2021-03-25 00:22:56 +0200

    Build arginfo using stub

---
+++ jsonpath.stub.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * @generate-function-entries
+ * @generate-legacy-arginfo
+ */
+
+class JsonPath
+{
+    /**
+     * @param array $data
+     * @param string $expression
+     *
+     * @return array|bool
+     */
+    public function find(array $data, string $expression): array|bool;
+}
