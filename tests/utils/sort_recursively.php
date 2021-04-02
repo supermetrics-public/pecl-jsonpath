2512438d6e2353578c5589d2729aa6042f798c94
Viktor Djupsjöbacka <viktor.djupsjobacka@supermetrics.com>
2021-04-03 00:37:54 +0300

    Add parameter type hint and PHPDoc

--- tests/utils/sort_recursively.php
+++ tests/utils/sort_recursively.php
@@ -1,12 +1,16 @@
 <?php
 
-function sortRecursively(&$array): void
+/**
+ * Sort an array by its values, recursively
+ * @param array &$array
+ */
+function sortRecursively(array &$array): void
 {
     // Sequential array, re-index after sorting
     if (array_keys($array) === range(0, count($array) - 1)) {
         sort($array);
     }
-    // Associative, maintain keys
+    // Associative array, maintain keys
     else {
         asort($array);
     }
