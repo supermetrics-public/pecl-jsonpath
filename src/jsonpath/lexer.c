950adc1565228d047f0aceeec9a6ca440985d345
Mike Kaminski <michael.w.kaminski@gmail.com>
2021-04-27 21:17:09 -0400

    Sort lex tokens enum

--- src/jsonpath/lexer.c
+++ src/jsonpath/lexer.c
@@ -21,32 +21,32 @@ static bool extract_unbounded_numeric_literal(char* p, char* buffer, size_t bufS
 static bool extract_boolean_literal(char* p, char* buffer, size_t bufSize, char* json_path);
 
 const char* LEX_STR[] = {
-    "LEX_NOT_FOUND",       /* Token not found */
-    "LEX_ROOT",            /* $ */
-    "LEX_CUR_NODE",        /* @ */
-    "LEX_WILD_CARD",       /* * */
-    "LEX_DEEP_SCAN",       /* .. */
-    "LEX_NODE",            /* .child, ['child'] */
-    "LEX_EXPR_END",        /* ] */
-    "LEX_SLICE",           /* : */
+    "LEX_AND",             /* && */
     "LEX_CHILD_SEP",       /* , */
-    "LEX_EXPR_START",      /* ? */
+    "LEX_CUR_NODE",        /* @ */
+    "LEX_DEEP_SCAN",       /* .. */
     "LEX_EQ",              /* == */
-    "LEX_NEQ",             /* != */
-    "LEX_LT",              /* < */
-    "LEX_LTE",             /* <= */
+    "LEX_ERR"              /* Signals lexing error */
+    "LEX_EXPR_END",        /* ] */
+    "LEX_EXPR_START",      /* ? */
+    "LEX_FILTER_START",    /* [ */
     "LEX_GT",              /* > */
     "LEX_GTE",             /* >= */
-    "LEX_RGXP",            /* =~ */
-    "LEX_PAREN_OPEN",      /* ( */
-    "LEX_PAREN_CLOSE",     /* ) */
-    "LEX_LITERAL",         /* "some string" 'some string' */
     "LEX_LITERAL_BOOL",    /* true, false */
     "LEX_LITERAL_NUMERIC", /* long, double */
-    "LEX_FILTER_START",    /* [ */
-    "LEX_AND",             /* && */
+    "LEX_LITERAL",         /* "some string" 'some string' */
+    "LEX_LT",              /* < */
+    "LEX_LTE",             /* <= */
+    "LEX_NEQ",             /* != */
+    "LEX_NODE",            /* .child, ['child'] */
+    "LEX_NOT_FOUND",       /* Token not found */
     "LEX_OR",              /* || */
-    "LEX_ERR"              /* Signals lexing error */
+    "LEX_PAREN_CLOSE",     /* ) */
+    "LEX_PAREN_OPEN",      /* ( */
+    "LEX_RGXP",            /* =~ */
+    "LEX_ROOT",            /* $ */
+    "LEX_SLICE",           /* : */
+    "LEX_WILD_CARD",       /* * */
 };
 
 void raise_error(const char* msg, char* json_path, char* cur_pos) {
--- src/jsonpath/lexer.h
+++ src/jsonpath/lexer.h
@@ -4,32 +4,32 @@
 #include <stddef.h>
 
 typedef enum {
-  LEX_NOT_FOUND,       /* Token not found */
-  LEX_ROOT,            /* $ */
-  LEX_CUR_NODE,        /* @ */
-  LEX_WILD_CARD,       /* * */
-  LEX_DEEP_SCAN,       /* .. */
-  LEX_NODE,            /* .child, ['child'] */
-  LEX_EXPR_END,        /* ] */
-  LEX_SLICE,           /* : */
+  LEX_AND,             /* && */
   LEX_CHILD_SEP,       /* , */
-  LEX_EXPR_START,      /* ? */
+  LEX_CUR_NODE,        /* @ */
+  LEX_DEEP_SCAN,       /* .. */
   LEX_EQ,              /* == */
-  LEX_NEQ,             /* != */
-  LEX_LT,              /* < */
-  LEX_LTE,             /* <= */
+  LEX_ERR,             /* Signals lexing error */
+  LEX_EXPR_END,        /* ] */
+  LEX_EXPR_START,      /* ? */
+  LEX_FILTER_START,    /* [ */
   LEX_GT,              /* > */
   LEX_GTE,             /* >= */
-  LEX_RGXP,            /* =~ */
-  LEX_PAREN_OPEN,      /* ( */
-  LEX_PAREN_CLOSE,     /* ) */
-  LEX_LITERAL,         /* "some string" 'some string' */
   LEX_LITERAL_BOOL,    /* true, false */
   LEX_LITERAL_NUMERIC, /* int, float */
-  LEX_FILTER_START,    /* [ */
-  LEX_AND,             /* && */
+  LEX_LITERAL,         /* "some string" 'some string' */
+  LEX_LT,              /* < */
+  LEX_LTE,             /* <= */
+  LEX_NEQ,             /* != */
+  LEX_NODE,            /* .child, ['child'] */
+  LEX_NOT_FOUND,       /* Token not found */
   LEX_OR,              /* || */
-  LEX_ERR              /* Signals lexing error */
+  LEX_PAREN_CLOSE,     /* ) */
+  LEX_PAREN_OPEN,      /* ( */
+  LEX_RGXP,            /* =~ */
+  LEX_ROOT,            /* $ */
+  LEX_SLICE,           /* : */
+  LEX_WILD_CARD        /* * */
 } lex_token;
 
 extern const char* LEX_STR[];
