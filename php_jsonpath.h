c430462ee482eb768058cf788341c9caca75907e
Viktor Djupsjöbacka <viktor.djupsjobacka@supermetrics.com>
2025-01-29 15:00:54 +0200

    Publish version 3.0.0

--- php_jsonpath.h
+++ php_jsonpath.h
@@ -24,7 +24,7 @@
 extern zend_module_entry jsonpath_module_entry;
 #define phpext_jsonpath_ptr &jsonpath_module_entry
 
-#define PHP_JSONPATH_VERSION "2.1.1"
+#define PHP_JSONPATH_VERSION "3.0.0"
 
 #ifdef PHP_WIN32
 #define PHP_JSONPATH_API __declspec(dllexport)
